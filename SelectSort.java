c54427699242929349e78663c77aa64a70581a84
chenyichuang <122584027+chenyichuang@users.noreply.github.com>
2023-01-14 16:31:56 +0800

    min創建一個java文件

---
+++ SelectSort.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class SelectSort {
+	public static void main(String[] args) {
+		int[] arr = new int[] { 5, 2, 1, 9, 4, 10, 6, 7, 6, 1, 13, 36 };
+		selectSort(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	
+	//選擇排序
+	public static void selectSort(int[] arr) {
+		//依序走訪陣列中的每個數
+		for (int i = 0; i < arr.length; i++) {
+			int minIndex = i;
+			//依序比較陣列中，每個數的後面所有數，紀錄最小數的位置
+			for (int j = i + 1; j < arr.length; j++) {
+				//只要循環到後方的數，小於原紀錄最小的數
+				if (arr[minIndex] > arr[j]) {
+					//就把最新一次紀錄的最小數存到minIndex
+					minIndex = j;
+				}
+			}
+			
+			//如果  minIndex 不等於 i，表示i位置並不是最小，把i位置跟minIndex位置調換
+			if (minIndex > i) {
+				int temp = arr[i];
+				arr[i] = arr[minIndex];
+				arr[minIndex] = temp;
+			}
+
+		}
+
+	}
+
+}
